ab14717874d8fb0683072568944e8783d1d0b668
timbotimber <hitimstephens@gmail.com>
2020-02-29 17:55:07 +0100

    google code

--- server-code/passport/google.js
+++ server-code/passport/google.js
@@ -1,5 +1,5 @@
 const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const GoogleStrategy = require("passport-google-oauth20");
 const User = require("../models/User");
 
 passport.use(
