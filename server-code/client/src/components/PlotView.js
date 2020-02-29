95b8cb6159d7a6df9e6ac867f2a1bc960c5de25f
Jason Franklin <jfrank325@gmail.com>
2020-02-29 15:50:50 +0100

    trip review as dropdown, title updating and saving everything to db

--- server-code/client/src/components/PlotView.js
+++ server-code/client/src/components/PlotView.js
@@ -1,18 +1,18 @@
-import React from "react";
-import mapboxgl from "mapbox-gl";
-import MapBoxGLDraw from "mapbox-gl-draw";
-import axios from "axios";
-import { Link } from "react-router-dom";
-import TripReview from "./TripReview";
+import React from 'react';
+import mapboxgl from 'mapbox-gl';
+import MapBoxGLDraw from 'mapbox-gl-draw';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import TripReview from './TripReview';
 
 mapboxgl.accessToken =
-  "pk.eyJ1IjoiamFjcXVlbGluZWNoZW4iLCJhIjoiY2s2ZHB5Y2RxMDkxbzNkbXA2bXVzM3JvbiJ9.pUyDxtMWjGqmGgX4JAdL7g";
+  'pk.eyJ1IjoiamFjcXVlbGluZWNoZW4iLCJhIjoiY2s2ZHB5Y2RxMDkxbzNkbXA2bXVzM3JvbiJ9.pUyDxtMWjGqmGgX4JAdL7g';
 
 export default class PlotView extends React.Component {
   // constructor(props) {
   //   super(props);
   state = {
-    title: "",
+    title: '',
     origin: [],
     destination: [],
     map: null,
@@ -20,19 +20,19 @@ export default class PlotView extends React.Component {
     lng: 5,
     lat: 34,
     zoom: 4,
-    uuid: "",
-    duration: "",
-    distance: "",
+    uuid: '',
+    duration: '',
+    distance: '',
     coordinates: [],
     waypoints: [],
-    reviewTrip: false
+    reviewTrip: false,
   };
 
   //
   removeRoute = () => {
-    if (this.state.map.getSource("route")) {
-      this.state.map.removeLayer("route");
-      this.state.map.removeSource("route");
+    if (this.state.map.getSource('route')) {
+      this.state.map.removeLayer('route');
+      this.state.map.removeSource('route');
       //  document.getElementById('calculated-line').innerHTML = '';
     } else {
       return;
@@ -42,27 +42,27 @@ export default class PlotView extends React.Component {
   updateRoute = () => {
     this.removeRoute(); // overwrite any existing layers
     let data = this.state.draw.getAll();
-    let answer = document.getElementById("calculated-line");
+    let answer = document.getElementById('calculated-line');
     let lastFeature = data.features.length - 1;
     let coords = data.features[lastFeature].geometry.coordinates;
-    let newCoords = coords.join(";");
+    let newCoords = coords.join(';');
     this.getMatch(newCoords);
   };
 
   getMatch = e => {
     let url =
-      "https://api.mapbox.com/directions/v5/mapbox/cycling/" +
+      'https://api.mapbox.com/directions/v5/mapbox/cycling/' +
       e +
-      "?geometries=geojson&steps=true&&access_token=" +
+      '?geometries=geojson&steps=true&&access_token=' +
       mapboxgl.accessToken;
 
     let req = new XMLHttpRequest();
-    console.log("req", req);
-    req.responseType = "json";
-    req.open("GET", url, true);
+    console.log('req', req);
+    req.responseType = 'json';
+    req.open('GET', url, true);
     req.onload = () => {
       let jsonResponse = req.response;
-      console.log("jsonReponse", jsonResponse);
+      console.log('jsonReponse', jsonResponse);
       this.setState(
         {
           distance: jsonResponse.routes[0].distance * 0.001,
@@ -71,7 +71,7 @@ export default class PlotView extends React.Component {
           uuid: jsonResponse.uuid,
           waypoints: jsonResponse.waypoints,
           origin: jsonResponse.routes[0].geometry.coordinates[0],
-          destination: jsonResponse.routes[0].geometry.coordinates.length - 1
+          destination: jsonResponse.routes[0].geometry.coordinates.length - 1,
         },
         () => console.log(this.state)
       );
@@ -91,46 +91,44 @@ export default class PlotView extends React.Component {
 
   addRoute = coords => {
     // check if the route is already loaded
-    if (this.state.map.getSource("route")) {
-      this.state.map.removeLayer("route");
-      this.state.map.removeSource("route");
+    if (this.state.map.getSource('route')) {
+      this.state.map.removeLayer('route');
+      this.state.map.removeSource('route');
     } else {
       this.state.map.addLayer({
-        id: "route",
-        type: "line",
+        id: 'route',
+        type: 'line',
         source: {
-          type: "geojson",
+          type: 'geojson',
           data: {
-            type: "Feature",
+            type: 'Feature',
             properties: {},
-            geometry: coords
-          }
+            geometry: coords,
+          },
         },
         layout: {
-          "line-join": "round",
-          "line-cap": "round"
+          'line-join': 'round',
+          'line-cap': 'round',
         },
         paint: {
-          "line-color": "#3b9ddd",
-          "line-width": 8,
-          "line-opacity": 0.8
-        }
+          'line-color': '#3b9ddd',
+          'line-width': 8,
+          'line-opacity': 0.8,
+        },
       });
     }
   };
 
   getInstructions = data => {
     // Target the sidebar to add the instructions
-    let directions = document.getElementById("directions");
+    let directions = document.getElementById('directions');
     let legs = data.legs;
     let tripDirections = [];
     // Output the instructions for each step of each leg in the response object
     for (let i = 0; i < legs.length; i++) {
       let steps = legs[i].steps;
       for (let j = 0; j < steps.length; j++) {
-        tripDirections.push(
-          "<br><li>" + steps[j].maneuver.instruction + "</li>"
-        );
+        tripDirections.push('<br><li>' + steps[j].maneuver.instruction + '</li>');
       }
     }
     // directions.innerHTML = '<br><h2>Trip duration: ' + Math.floor(data.duration / 60) + ' min.</h2>' + tripDirections;
@@ -139,72 +137,58 @@ export default class PlotView extends React.Component {
   componentDidMount = () => {
     const map = new mapboxgl.Map({
       container: this.mapContainer,
-      style: "mapbox://styles/mapbox/streets-v11",
+      style: 'mapbox://styles/mapbox/streets-v11',
       center: [this.state.lng, this.state.lat],
       zoom: this.state.zoom,
       duration: this.state.duration,
-      distance: this.state.distance
+      distance: this.state.distance,
     });
     // console.log('duration', duration);
     const draw = new MapBoxGLDraw({
       displayControlsDefault: false,
       controls: {
         line_string: true,
-        trash: true
+        trash: true,
       },
       styles: [
         // ACTIVE (being drawn)
         // line stroke
         {
-          id: "gl-draw-line",
-          type: "line",
-          filter: [
-            "all",
-            ["==", "$type", "LineString"],
-            ["!=", "mode", "static"]
-          ],
+          id: 'gl-draw-line',
+          type: 'line',
+          filter: ['all', ['==', '$type', 'LineString'], ['!=', 'mode', 'static']],
           layout: {
-            "line-cap": "round",
-            "line-join": "round"
+            'line-cap': 'round',
+            'line-join': 'round',
           },
           paint: {
-            "line-color": "#3b9ddd",
-            "line-dasharray": [0.2, 2],
-            "line-width": 4,
-            "line-opacity": 0.7
-          }
+            'line-color': '#3b9ddd',
+            'line-dasharray': [0.2, 2],
+            'line-width': 4,
+            'line-opacity': 0.7,
+          },
         },
         // vertex point halos
         {
-          id: "gl-draw-polygon-and-line-vertex-halo-active",
-          type: "circle",
-          filter: [
-            "all",
-            ["==", "meta", "vertex"],
-            ["==", "$type", "Point"],
-            ["!=", "mode", "static"]
-          ],
+          id: 'gl-draw-polygon-and-line-vertex-halo-active',
+          type: 'circle',
+          filter: ['all', ['==', 'meta', 'vertex'], ['==', '$type', 'Point'], ['!=', 'mode', 'static']],
           paint: {
-            "circle-radius": 10,
-            "circle-color": "#FFF"
-          }
+            'circle-radius': 10,
+            'circle-color': '#FFF',
+          },
         },
         // vertex points
         {
-          id: "gl-draw-polygon-and-line-vertex-active",
-          type: "circle",
-          filter: [
-            "all",
-            ["==", "meta", "vertex"],
-            ["==", "$type", "Point"],
-            ["!=", "mode", "static"]
-          ],
+          id: 'gl-draw-polygon-and-line-vertex-active',
+          type: 'circle',
+          filter: ['all', ['==', 'meta', 'vertex'], ['==', '$type', 'Point'], ['!=', 'mode', 'static']],
           paint: {
-            "circle-radius": 6,
-            "circle-color": "#3b9ddd"
-          }
-        }
-      ]
+            'circle-radius': 6,
+            'circle-color': '#3b9ddd',
+          },
+        },
+      ],
     });
 
     this.setState({ map, draw }, () => {
@@ -214,16 +198,16 @@ export default class PlotView extends React.Component {
       map.addControl(draw);
 
       // add create, update, or delete actions
-      map.on("draw.create", this.updateRoute);
-      map.on("draw.update", this.updateRoute);
-      map.on("draw.delete", this.removeRoute);
+      map.on('draw.create', this.updateRoute);
+      map.on('draw.update', this.updateRoute);
+      map.on('draw.delete', this.removeRoute);
 
       // Storing new coordinate (whatever the user interacts with)
-      map.on("move", () => {
+      map.on('move', () => {
         this.setState({
           lng: map.getCenter().lng.toFixed(4),
           lat: map.getCenter().lat.toFixed(4),
-          zoom: map.getZoom().toFixed(2)
+          zoom: map.getZoom().toFixed(2),
         });
       });
     });
@@ -231,25 +215,35 @@ export default class PlotView extends React.Component {
 
   goToReviewTrip = () => {
     this.setState({
-      reviewTrip: !this.state.reviewTrip
+      reviewTrip: !this.state.reviewTrip,
+    });
+  };
+
+  updateTitle = text => {
+    this.setState({
+      title: text,
     });
   };
 
   render() {
     console.log(this.state);
+    let tripReviewCard;
+    let text;
     if (this.state.reviewTrip) {
-      return <TripReview tripData={this.state} />;
+      text = 'Go Back';
+      tripReviewCard = <TripReview tripData={this.state} updateTitle={this.updateTitle} />;
+    } else {
+      text = 'Review Trip';
     }
 
     return (
       <div>
         <div className="sidebarStyle">
           <div>
-            Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom:{" "}
-            {this.state.zoom}
+            Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom: {this.state.zoom}
           </div>
-
-          <button onClick={this.goToReviewTrip}>Review trip</button>
+          {tripReviewCard}
+          <button onClick={this.goToReviewTrip}>{text}</button>
         </div>
         <div ref={el => (this.mapContainer = el)} className="mapContainer" />
       </div>
--- server-code/client/src/components/TripReview.js
+++ server-code/client/src/components/TripReview.js
@@ -1,18 +1,11 @@
-import React from "react";
-import axios from "axios";
-import { Link, Redirect } from "react-router-dom";
-import { Component } from "react";
+import React from 'react';
+import axios from 'axios';
+import { Link, Redirect } from 'react-router-dom';
+import { Component } from 'react';
 
-class TripReview extends React.Component {
-  state = {
-    title: "",
-    redirect: false
-  };
-
-  handleChange = event => {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
+const TripReview = props => {
+  const handleChange = event => {
+    props.updateTitle(event.target.value);
   };
 
   //   handleSubmit = event => {
@@ -23,42 +16,10 @@ class TripReview extends React.Component {
   //     // change state.redirect to true
   //   };
 
-  handleSubmit = event => {
+  // console.log('trip data here:', this.props.tripData);
+
+  const handleSubmit = event => {
     event.preventDefault();
-    const {
-      origin,
-      destination,
-      lng,
-      lat,
-      zoom,
-      distance,
-      duration,
-      coordinates,
-      uuid,
-      waypoints
-    } = this.state;
-    // const id = this.state.match.params.
-    axios
-      .post("/api/trips/addTrip", {
-        origin,
-        destination,
-        lng,
-        lat,
-        zoom,
-        distance,
-        duration,
-        coordinates,
-        uuid,
-        waypoints
-      })
-      .then(response => {
-        console.log(response);
-      });
-  };
-
-  render() {
-    console.log("trip data here:", this.props.tripData);
-
     const {
       title,
       origin,
@@ -70,33 +31,59 @@ class TripReview extends React.Component {
       duration,
       coordinates,
       uuid,
-      waypoints
-    } = this.props.tripData;
-    // if this.state.redirect --> return <Redirect to="/profile" />
-    return (
-      <div>
-        <form className="review-trip" onSubmit={this.handleSubmit}>
-          <h1>Review your trip</h1>
-          <label htmlFor="title">Trip name</label>
-          <input
-            id="title"
-            name="title"
-            value={this.state.title}
-            onChange={this.handleChange}
-            type="text"
-            placeholder="Name your trip"
-          />
-          <div>
-            <p>Origin: {origin}</p>
-            <p>Destination: {destination}</p>
-            <p>Duration: {duration}</p>
-            <p>Distance: {distance}</p>
-            <button>Save this trip</button>
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+      waypoints,
+    } = props.tripData;
+    // const id = this.state.match.params.
+    axios
+      .post('/api/trips/addTrip', {
+        title,
+        origin,
+        destination,
+        lng,
+        lat,
+        zoom,
+        distance,
+        duration,
+        coordinates,
+        uuid,
+        waypoints,
+      })
+      .then(response => {
+        console.log(response);
+      });
+  };
+
+  const {
+    title,
+    origin,
+    destination,
+    lng,
+    lat,
+    zoom,
+    distance,
+    duration,
+    coordinates,
+    uuid,
+    waypoints,
+  } = props.tripData;
+  // if this.state.redirect --> return <Redirect to="/profile" />
+  return (
+    <div>
+      <form className="review-trip" onSubmit={handleSubmit}>
+        <h1>Review your trip</h1>
+        <label htmlFor="title">Trip name</label>
+        <input id="title" name="title" value={title} onChange={handleChange} type="text" placeholder="Name your trip" />
+        <div>
+          <p>Origin: {origin}</p>
+          <p>Destination: {destination}</p>
+          <p>Duration: {duration}</p>
+          <p>Distance: {distance}</p>
+          {/* <p>Waypoints: {waypoints}</p> */}
+          <button>Save this trip</button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default TripReview;
