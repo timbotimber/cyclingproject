73d5fdb040b745296ce6855866478cd69bce1c81
Jacqueline Chen <xjacquelinechen@gmail.com>
2020-03-02 15:45:11 +0100

    Implemented reverse geocoding on ReviewTrip and adding origin and destination names to the Trip Model

--- server-code/models/Trip.js
+++ server-code/models/Trip.js
@@ -4,7 +4,9 @@ const Schema = mongoose.Schema;
 const tripSchema = new Schema({
   title: String,
   origin: Array, // First pair of coordinates
+  origin_name: String,
   destination: Array, // Last pair of coordinates --> currently storing the key instead of the coordinates array, need to fix!
+  destination_name: String,
   duration: Number, // Comes from Mapbox
   distance: Number, // Comes from Mapbox
   coordinates: Array,
--- server-code/routes/trips.js
+++ server-code/routes/trips.js
@@ -1,22 +1,33 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/User');
-const Trip = require('../models/Trip');
+const User = require("../models/User");
+const Trip = require("../models/Trip");
 
-router.get('/addTrip/:id/review', (req, res) => {
+router.get("/addTrip/:id/review", (req, res) => {
   Trip.findById(req.params.id)
     .then(trip => {
       res.json(trip);
     })
     .catch(err => {
       res.status(500).json({
-        message: err.message,
+        message: err.message
       });
     });
 });
 
-router.post('/addTrip', (req, res, next) => {
-  const { title, uuid, duration, distance, coordinates, waypoints, origin, destination } = req.body;
+router.post("/addTrip", (req, res, next) => {
+  const {
+    title,
+    uuid,
+    duration,
+    distance,
+    coordinates,
+    waypoints,
+    origin,
+    origin_name,
+    destination,
+    destination_name
+  } = req.body;
   Trip.create({
     title,
     uuid,
@@ -25,7 +36,9 @@ router.post('/addTrip', (req, res, next) => {
     coordinates,
     waypoints,
     origin,
+    origin_name,
     destination,
+    destination_name
   }).then(postTrip => {
     res.json(postTrip);
   });
