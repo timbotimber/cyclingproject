6a78651a22600270319f343630aeb1f0ef7fe74a
Jacqueline Chen <xjacquelinechen@gmail.com>
2020-03-03 17:41:52 +0100

    Updated navbar, colors and type

--- server-code/client/src/components/NavBar.js
+++ server-code/client/src/components/NavBar.js
@@ -36,7 +36,7 @@ const NavBar = props => {
           <a href="/">Sykkel</a>
         </div>
 
-        <div className="account-links">
+        <div className="nav-links">
           <Link to="/login">Log in</Link>
           <Link to="/signup">Sign up</Link>
           <Link to="/plotview">Plan</Link>
