32eff033e6432cc7bf1f5ee18aeeee342a340394
timbotimber <hitimstephens@gmail.com>
2020-03-03 17:41:42 +0100

    more server stuff

---
+++ server-code/client/build/precache-manifest.29ae00ea462d9530c5e0e1a8f353133b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b82bdb921296a53cc3f7349da58e509",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09cf8fac9a7a6d58d5d1",
+    "url": "/static/css/main.1210a5af.chunk.css"
+  },
+  {
+    "revision": "639ef19bb7a0a1546669",
+    "url": "/static/js/2.3d5d46b2.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.3d5d46b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09cf8fac9a7a6d58d5d1",
+    "url": "/static/js/main.95898110.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  }
+]);
