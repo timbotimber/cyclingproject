67f6c933f4b636f8b64ce730a675bf760735e126
Jacqueline Chen <xjacquelinechen@gmail.com>
2020-03-03 15:30:35 +0100

    Added filter functionality (not working yet), and modified search bar on the /trips page

--- server-code/client/src/components/FilterPanel.js
+++ server-code/client/src/components/FilterPanel.js
@@ -4,11 +4,55 @@ import Search from "./Search";
 export default class FilterPanel extends React.Component {
   render() {
     return (
-      <Search
-        updateSearchText={this.props.updateSearchText}
-        query={this.props.query}
-        executeSearch={this.props.executeSearch}
-      />
+      <div className="filter-panel">
+        {/* <p className="caption-strong">Search for a trip</p> */}
+        <Search
+          updateSearchText={this.props.updateSearchText}
+          query={this.props.query}
+          executeSearch={this.props.executeSearch}
+        />
+        <p className="caption-strong">Difficulty</p>
+        <form>
+          <label htmlFor="Easy">
+            <input
+              className="checkbox"
+              type="checkbox"
+              name="Easy"
+              id="Easy"
+              checked={this.props.easy}
+              onChange={this.props.mutate}
+            />
+            Easy
+          </label>
+
+          <label htmlFor="Intermediate">
+            <input
+              className="checkbox"
+              type="checkbox"
+              name="Intermediate"
+              id="Intermediate"
+              checked={this.props.intermediate}
+              onChange={this.props.mutate}
+            />
+            Intermediate
+          </label>
+
+          <label htmlFor="Advanced">
+            <input
+              className="checkbox"
+              type="checkbox"
+              name="Advanced"
+              id="Advanced"
+              checked={this.props.advanced}
+              onChange={this.props.mutate}
+            />
+            Advanced
+          </label>
+          <button className="search-button" onClick={this.props.executeFilter}>
+            Apply filters
+          </button>
+        </form>
+      </div>
     );
   }
 }
