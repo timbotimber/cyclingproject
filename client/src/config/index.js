4a25b09ebd08d3cec0f4156fee25292fbe65a9a0
Martin Eskdale Moen <martinmoen@gmail.com>
2020-04-22 12:20:22 +0100

    Fix url for client

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -2,7 +2,7 @@ import mapBoxStyling from "./mapbox_styling";
 
 export default {
   api: {
-    host: "http://127.0.0.1:5000"
+    host: `${process.env.REACT_APP_SERVER_URL}`,
   },
   mapBoxStyling
 };
